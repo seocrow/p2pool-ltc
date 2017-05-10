acba7187d03e3a91e6791bbaad63c16f4029e8d2
ilsawa <ils2000@mail.ru>
2017-05-11 02:27:36 +0600

    first commit

---
+++ p2pool/networks/litecoin.py
@@ -0,0 +1,21 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['litecoin']
+SHARE_PERIOD = 15 # seconds
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 200 # shares
+SPREAD = 3 # blocks
+IDENTIFIER = 'e037d5b8c6923410'.decode('hex')
+PREFIX = '7208c1a53ef629b0'.decode('hex')
+P2P_PORT = 9338
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = False
+WORKER_PORT = 9327
+BOOTSTRAP_ADDRS = 'crypto.office-on-the.net'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-ltc'
+VERSION_CHECK = lambda v: None if 100400 <= v else 'Litecoin version too old. Upgrade to 0.10.4 or newer!'
+VERSION_WARNING = lambda v: None
+SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
+SEGWIT_ACTIVATION_VERSION = 16
